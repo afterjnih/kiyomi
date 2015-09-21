f3de1f6633316cace7fb3bd2a99342cbf5f5bdbf
Koji Ota <afterjnih@gmail.com>
2015-09-21 11:47:50 +0900

    change directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,28 @@ var electron = require('electron-connect').server.create();
 gulp.task('default', ['watch', 'babel_node', 'babel_client'], function(){
 });
 
-gulp.task('babel_node', function(){
-  return gulp.src('./src/node/*.es6')
+gulp.task('babel_browser', function(){
+  return gulp.src('./src/browser/*.es6')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./app/node/'));
+    .pipe(gulp.dest('./app/browser/'));
 });
 
-gulp.task('babel_client', function(){
-  return gulp.src('./src/client/*.es6')
+gulp.task('babel_renderer', function(){
+  return gulp.src('./src/renderer/*.es6')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./app/client/'));
+    .pipe(gulp.dest('./app/renderer/'));
+});
+
+gulp.task('babel_app', function(){
+  return gulp.src('./src/app.es6')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./app/'));
 });
 
 gulp.task('packaging', function(){
@@ -34,9 +42,9 @@ gulp.task('watch', function(){
   gulp.watch('./src/**/*.es6', ['babel_node', 'babel_client']);
 });
 
-gulp.task('start', ['babel_node', 'babel_client'], function ()  {
+gulp.task('start', ['babel_browser', 'babel_renderer', 'babel_app'], function ()  {
   electron.start();
-  gulp.watch('./src/**/*.es6', ['babel_node', 'babel_client']);
-  gulp.watch(['./app/node/index.js','./app/client/*.js'], electron.restart);
-  gulp.watch(['./app/index.html'], electron.reload);
+  gulp.watch('./src/**/*.es6', ['babel_browser', 'babel_renderer']);
+  gulp.watch(['./app/app.js','./app/browser/*.js'], electron.restart);
+  gulp.watch(['./app/index.html', './app/renderer/*.js'], electron.reload);
 });
